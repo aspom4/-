96f4fc610af4b98182d55b9bebea96363192d572
aspom4 <as_pomazan@mail.ru>
2024-06-07 16:48:29 +0900

    Add files via upload

---
+++ resolve2_2.py
@@ -0,0 +1,30 @@
+print('Задание №1 "Длина строки"')
+a = (len('Язык программирования PYTHON'))
+print('Ответ:', a, 'символов')
+print('Задание №2 "Суммы и разности"')
+first = 15
+second = 17
+summa = (first+second)
+print('Сумма =', summa)
+diff = (first-second)
+print('Разность = ', diff)
+
+print('Задание "Среднее арифметическое"')
+first = 17
+second = 15
+third = 3
+mean = ((17+15+3)/3)
+print('Среднее арифметическое = ', mean)
+
+print('Задание "Простые строчки"')
+First_string = "Вторник"
+Second_string = "Понедельник"
+print(Second_string, ',', ' ', First_string)
+print('Задача "Сложная формула"')
+a = 3
+b = 5
+c = 7
+f = ((a*b)+(a*c))
+print('Ответ1:', int(f))
+print('Ответ2:', int(f**3/2))
+print(str('Конец заданиям по вводному уроку'))
